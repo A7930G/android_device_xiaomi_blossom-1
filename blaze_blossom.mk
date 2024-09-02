b1f9ae308d67c3cfd0a135c86f96e3433e6eadf8
AsTechpro20 <94213632+AsTechpro20@users.noreply.github.com>
2024-09-02 21:27:22 +0530

    Update and rename lineage_blossom.mk to blaze_blossom.mk

--- blaze_blossom.mk
+++ blaze_blossom.mk
@@ -12,12 +12,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/xiaomi/blossom/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common ProjectBlaze stuff.
+$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
 
 scr_resolution := 720
 
-PRODUCT_NAME := lineage_blossom
+# ProjectBlaze stuffs.
+BLAZE_MAINTAINER := AsTechpro20
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_NAME := blaze_blossom
 PRODUCT_DEVICE := blossom
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
