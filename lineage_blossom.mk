aaa7413e623ebd24b475faa6f1520f3eecac360a
AsTechpro20 <94213632+AsTechpro20@users.noreply.github.com>
2024-09-18 21:53:14 +0530

    Update lineage_blossom.mk

--- lineage_blossom.mk
+++ lineage_blossom.mk
@@ -17,16 +17,6 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 scr_resolution := 720
 
-# Matrixx stuff.
-MATRIXX_MAINTAINER := AsTechpro20
-MATRIXX_CHIPSET := MT6765
-MATRIXX_BATTERY := 5000mAh
-MATRIXX_DISPLAY := 720x1600
-USE_PIXEL_CHARGING := true
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_SUPPORTS_BLUR := true
-
 PRODUCT_NAME := lineage_blossom
 PRODUCT_DEVICE := blossom
 PRODUCT_MANUFACTURER := Xiaomi
