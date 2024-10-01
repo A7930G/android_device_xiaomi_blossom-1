e019187891fda16635dbbda9388f3747c05da550
AsTechpro20 <94213632+AsTechpro20@users.noreply.github.com>
2024-10-01 07:57:38 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,8 +6,13 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/voltage_blossom.mk
+    $(LOCAL_DIR)/lineage_blossom.mk
 
 COMMON_LUNCH_CHOICES := \
     voltage_blossom-user \
     voltage_blossom-userdebug \
     voltage_blossom-eng
+    lineage_blossom-user \
+    lineage_blossom-userdebug \
+    lineage_blossom-eng
+
