4502fb1755a1ecb6670b1792791165abc72555cb
AsTechpro20 <94213632+AsTechpro20@users.noreply.github.com>
2024-10-02 13:42:35 +0530

    Update and rename lineage_blossom.mk to euclid_blossom.mk

--- euclid_blossom.mk
+++ euclid_blossom.mk
@@ -12,12 +12,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/xiaomi/blossom/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common EuclidOS stuff.
+$(call inherit-product, vendor/euclid/config/common_full_phone.mk)
 
 scr_resolution := 720
 
-PRODUCT_NAME := lineage_blossom
+# EuclidOS Stuffs.
+EUCLID_MAINTAINER := AsTechpro20
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_NAME := euclid_blossom
 PRODUCT_DEVICE := blossom
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
