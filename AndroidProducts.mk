500ad5a78c66d69dc1d8e42ed3b643c01b6f091f
AsTechpro20 <94213632+AsTechpro20@users.noreply.github.com>
2024-10-02 13:42:57 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_blossom.mk
+    $(LOCAL_DIR)/euclid_blossom.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_blossom-user \
-    lineage_blossom-userdebug \
-    lineage_blossom-eng
+    euclid_blossom-user \
+    euclid_blossom-userdebug \
+    euclid_blossom-eng
