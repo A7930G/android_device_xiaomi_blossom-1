94b05e9f64a59cbdbe4ace6a396002be1544cab4
AsTechpro20 <94213632+AsTechpro20@users.noreply.github.com>
2024-10-05 18:01:08 +0530

    Update lineage_blossom.mk

--- lineage_blossom.mk
+++ lineage_blossom.mk
@@ -16,9 +16,16 @@ $(call inherit-product, device/xiaomi/blossom/device.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 scr_resolution := 720
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_BLUR := true
+TARGET_ENABLE_BLUR := true
 
-#AlphaDroid stuffs.
-ALPHA_MAINTAINER := AsTechpro20
+# RisingOS stuffs.
+RISING_CHIPSET="MediaTek 6765"
+RISING_MAINTAINER="AsTechpro20"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RISING_CHIPSET="MediaTek 6765"
+    RISING_MAINTAINER="AsTechpro20"
 
 PRODUCT_NAME := lineage_blossom
 PRODUCT_DEVICE := blossom
