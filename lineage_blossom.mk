559a663db56c9317a10a51b470da6d2a94182b54
AsTechpro20 <94213632+AsTechpro20@users.noreply.github.com>
2024-10-09 15:53:12 +0530

    Update lineage_blossom.mk

--- lineage_blossom.mk
+++ lineage_blossom.mk
@@ -12,17 +12,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/xiaomi/blossom/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common ProjectBlaze-14 stuff.
+$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
 
 scr_resolution := 720
 
-#AlphaDroid stuffs.
-ALPHA_MAINTAINER := AsTechpro20
+#ProjectBlaze-14 stuffs.
+BLAZE_MAINTAINER := AsTechpro20
 TARGET_SUPPORTS_BLUR := true
 TARGET_ENABLE_BLUR := true
 
-PRODUCT_NAME := lineage_blossom
+PRODUCT_NAME := blaze_blossom
 PRODUCT_DEVICE := blossom
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
